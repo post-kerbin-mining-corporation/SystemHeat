92567d299718599eeda99003d08f70f957c94e55
Chris Adderley <cadderley@gmail.com>
2014-12-30 09:44:57 -0800

    Added heat storage part
    Basic implementation

--- SystemHeat/SystemHeat/ModuleGenericHeatStorage.cs
+++ SystemHeat/SystemHeat/ModuleGenericHeatStorage.cs
@@ -8,5 +8,60 @@ namespace SystemHeat
 {
     public class ModuleGenericHeatStorage: PartModule
     {
+        // Animation that plays with increasing heat
+        [KSPField(isPersistant = false)]
+        public string HeatAnimation;
+        
+        // Transform for animation mixing
+        [KSPField(isPersistant = false)]
+        public string HeatTransformName;
+
+         // Private variables
+        private AnimationState[] heatStates;
+        private Transform heatTransform;
+        private ModuleSystemHeat heatModule;
+        
+        
+        public override void OnStart(PartModule.StartState state)
+        {
+            heatModule = part.gameObject.GetComponent<ModuleSystemHeat>();
+
+            if (heatModule == null)
+            {
+                Utils.LogError("No SystemHeat Module on part!");
+                return;
+            }
+
+            // Set up animation
+            if (heatTransform != null && HeatAnimation != "")
+            {
+                heatStates = Utils.SetUpAnimation(HeatAnimation, part);
+                heatTransform = part.FindModelTransform(HeatTransformName);
+            
+                foreach (AnimationState heatState in heatStates)
+                {
+                    heatState.AddMixingTransform(heatTransform);
+                    heatState.blendMode = AnimationBlendMode.Blend;
+                    heatState.layer = 15;
+                    heatState.weight = 1.0f;
+                    heatState.enabled = true;
+                }
+            }
+        }
+        
+        public void Update()
+        {
+            if (HighLogic.LoadedScene == GameScenes.FLIGHT)
+            {
+                if (HeatAnimation != "" && heatModule != null)
+                {
+                    foreach (AnimationState state in heatStates)
+                    {
+                        state.normalizedTime = heatModule.PartHeatFraction;
+                    }
+                }
+            }
+        }
+            
     }
 }
