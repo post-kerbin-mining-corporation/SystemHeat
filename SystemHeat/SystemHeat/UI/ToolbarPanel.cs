fea256e0a6ac6567d516d05ebb2bedd8df1a3448
Chris <cadderley@gmail.com>
2020-12-24 12:49:45 -0800

    Prepare 0.3.2

--- SystemHeat/SystemHeat/UI/ToolbarPanel.cs
+++ SystemHeat/SystemHeat/UI/ToolbarPanel.cs
@@ -62,7 +62,7 @@ namespace SystemHeat.UI
       totalOutgoingFluxTitle = transform.FindDeepChild("HeatRejectionTitle").GetComponent<Text>();
 
       totalLoopsTitle = transform.FindDeepChild("LoopCountTitle").GetComponent<Text>();
-      simRateTitle = transform.FindDeepChild("SimRateLabel").GetComponent<Text>();
+      simRateTitle = transform.FindDeepChild("SimRateTitle").GetComponent<Text>();
       overlayToggleTitle = transform.FindDeepChild("OverlayLabel").GetComponent<Text>();
       craftStatsTitle = transform.FindDeepChild("StatsHeaderText").GetComponent<Text>();
       panelTitle = transform.FindDeepChild("PanelTitleText").GetComponent<Text>();
